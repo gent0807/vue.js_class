f6bc4fef4f580aac4757dd1ea74b2a74edae14d3
gent0807 <theloopholesub0807@gmail.com>
2022-04-20 23:45:53 +0900

    first commit

---
+++ vue-project/src/views/SampleView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+    <div>{{htmlString}}</div>
+    <div v-html="htmlString"></div>
+</template>
+<script>
+export default {
+    
+    data() {
+        return {
+            title:'안녕하세요!',
+            htmlString: '<p style="color:red;">This is a red string</p>'
+    }
+},
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeUnmount() {},
+    unmounted() {},
+    methods: {}
+}
+</script>
---
+++ vue-project/src/views/TestView.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>안녕</h1>
+  <div>
+    <input type="text" v-model="vm"> <br>
+    <input type="number" v-model="nm"> <br>
+    <textarea id="" cols="30" rows="10" v-model="message"></textarea>
+  </div>
+</template>
+<script>
+export default {
+  
+  data() {
+    return {
+      vm: '대한민국',
+      nm: 1045,
+      message : '여러 줄을 입력할 수 있는 공간입니다.'
+  }
+},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
