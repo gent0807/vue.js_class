8b3226694cc801f601984858629e2ffe5e04e886
gent0807 <theloopholesnk@gmail.com>
2022-05-19 16:34:05 +0900

    ddd

--- vutify_sam/sam01/src/App.vue
+++ vutify_sam/sam01/src/App.vue
@@ -4,14 +4,17 @@
   <p>{{count}}</p>
   <v-btn @click="count++">추가</v-btn>
   <HomeComponent></HomeComponent>
+  <FooterComponent></FooterComponent>
 </div>
 </template>
 
 <script>
 import HomeComponent from './HomeCome.vue'
+import FooterComponent from './FooterCom.vue'
 export default{
   components:{
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   },
   data(){
     return{
---
+++ vutify_sam/sam01/src/FooterCom.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{footerTitle}}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            footerTitle:'푸터 입니다.'
+        }
+
+    }
+}
+</script>
