6949b08c60049c1f544e407a393f5e853e381a41
gent0807 <theloopholesnk@gmail.com>
2022-05-19 16:13:03 +0900

    dddd

--- vutify_sam/sam01/src/App.vue
+++ vutify_sam/sam01/src/App.vue
@@ -1,11 +1,18 @@
 <template>
+<div>
   <h1>{{title}}</h1>
   <p>{{count}}</p>
   <v-btn @click="count++">추가</v-btn>
+  <HomeComponent></HomeComponent>
+</div>
 </template>
 
 <script>
+import HomeComponent from './HomeCome.vue'
 export default{
+  components:{
+    HomeComponent
+  },
   data(){
     return{
       title:'안녕하세요',
