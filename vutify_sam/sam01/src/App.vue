b567b37195870116bd6a5a1a0938c040165f38fb
gent0807 <theloopholesnk@gmail.com>
2022-05-19 15:58:57 +0900

    ddd

---
+++ vutify_sam/sam01/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{title}}</h1>
+  <p>{{count}}</p>
+  <v-btn @click="count++">추가</v-btn>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      title:'안녕하세요',
+      count:1
+    }
+  }
+}
+</script>
