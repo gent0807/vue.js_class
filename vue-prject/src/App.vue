3d8a1355db3982351dae8c75749ba3460d0979de
gent0807 <theloopholesnk@gmail.com>
2022-05-26 15:04:23 +0900

    ddd

---
+++ vue-prject/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-card>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      items: [
+        { title: 'ToDo', icon: 'mdi-checkbox-marked-circle-plus-outline' },
+        { title: 'About', icon: 'mdi-help-box' }
+      ],
+      right: null
+    }
+  }
+}
+</script>
