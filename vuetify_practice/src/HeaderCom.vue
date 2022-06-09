90ebdb94b2f12ef04c076b9f085c4c920df0169a
gent0807 <theloopholsnk@gmail.com>
2022-06-09 15:55:43 +0900

    xxxxx

---
+++ vuetify_practice/src/HeaderCom.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{headerTitle}}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            headerTitle:'헤더 입니다.'
+        }
+
+    }
+}
+</script>
