3d0d15ef6f2af8a36f1130bcfa99bb96efcb5978
gent0807 <theloopholsnk@gmail.com>
2022-06-09 14:16:53 +0900

    ddd

---
+++ 자료/todo리스트완성/todosam03/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">ToDo List </v-list-item-title>
+          <v-list-item-subtitle>Vuetify-Todo</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="a in items" :key="a.title" :to="a.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ a.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ a.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#fcb69f" dark src="playground.jpg" prominent>
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>오늘 할일 하기</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      {
+        title: "Todo",
+        icon: "mdi-checkbox-marked-circle-plus-outline",
+        to: "/",
+      },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+    right: null,
+  }),
+};
+</script>
